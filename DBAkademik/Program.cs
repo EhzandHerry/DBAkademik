b2bb249a9c5639731352ec9f49696f7ab15aa9c5
EhzandHerry <ehzand.herry1926@gmail.com>
2023-04-05 10:24:13 +0700

    membuat fungsi insert data

--- DBAkademik/Program.cs
+++ DBAkademik/Program.cs
@@ -25,6 +25,28 @@ namespace DBAkademik
                 Console.Read();
             }
         }
-        
+        public void InsertData(string nama, int id, string jurusan, string jeniskelamin, int notelp, string alamat)
+        {
+            
+            string connectionString = "Data Source=LAPTOP-G2F55ONU\\EHZANDHERRY;Initial Catalog=LAPTOP-G2F55ONU\\EHZANDHERRY;User ID=sa;Password=Conex999";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            
+            string sql = "INSERT INTO mahasiswa (id_mhs, nama_mhs, id_jurusan, jenis_kelamin, no_telepon, alamat) VALUES (@id_mhs, @nama_mhs, @jurusan, @jenis_kelamin, @notelp, @alamat)";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@nama", nama);
+            command.Parameters.AddWithValue("@jurusan", jurusan);
+            command.Parameters.AddWithValue("@jenis kelamin", jeniskelamin);
+            command.Parameters.AddWithValue("@no telpon", notelp);
+            command.Parameters.AddWithValue("@alamat", alamat);
+
+            
+            connection.Open();
+            int rowsAffected = command.ExecuteNonQuery();
+            connection.Close();
+        }
     }
 }
