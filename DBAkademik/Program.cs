a91e75e85a04c544ceaff63912e09f3e3e567735
EhzandHerry <ehzand.herry1926@gmail.com>
2023-04-05 10:16:03 +0700

    mengkoneksikan ke sql server

---
+++ DBAkademik/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAkademik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().koneksi();
+        }
+        public void koneksi()
+        {
+            using (
+                //membuat koneksi
+                SqlConnection conn = new SqlConnection("data source=LAPTOP-G2F55ONU\\EHZANDHERRY;user ID=sa;password=Conex999"
+                ))
+            {
+                conn.Open();
+                Console.Write("Koneksi Ke Data Base");
+                Console.Read();
+            }
+        }
+        
+    }
+}
